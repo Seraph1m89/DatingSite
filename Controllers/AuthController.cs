e7c5ef59401494c5ff79fa4683736cfb26b97357
seraph <kuderskiy.denis@gmail.com>
2018-07-17 01:20:57 +0300

    Add user to login information.

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Threading.Tasks;
+using AutoMapper;
 using DatingApp.API.Data;
 using DatingApp.API.Dto;
 using DatingApp.API.Models;
@@ -8,56 +9,61 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace DatingApp.API.Controllers
 {
-    [Route("api/[controller]")]
-    public class AuthController : Controller
-    {
-        private readonly IAuthRepository _authRepository;
-        private readonly ITokenService _tokenService;
+	[Route("api/[controller]")]
+	public class AuthController : Controller
+	{
+		private readonly IAuthRepository _authRepository;
+		private readonly ITokenService _tokenService;
+		private readonly IMapper _mapper;
 
-        public AuthController(IAuthRepository authRepository,
-                              ITokenService tokenService)
-        {
-            this._authRepository = authRepository;
-            this._tokenService = tokenService;
-        }
+		public AuthController(IAuthRepository authRepository,
+							  ITokenService tokenService,
+							  IMapper mapper)
+		{
+			_authRepository = authRepository;
+			_tokenService = tokenService;
+			_mapper = mapper;
+		}
 
-        [HttpPost("register")]
-        public async Task<IActionResult> Register([FromBody] UserRegisterDto userForRegisterDto)
-        {
-            if (string.IsNullOrWhiteSpace(userForRegisterDto.UserName))
-            {
-                userForRegisterDto.UserName = userForRegisterDto.UserName.ToLowerInvariant();
-            }
+		[HttpPost("register")]
+		public async Task<IActionResult> Register([FromBody] UserRegisterDto userForRegisterDto)
+		{
+			if (string.IsNullOrWhiteSpace(userForRegisterDto.UserName))
+			{
+				userForRegisterDto.UserName = userForRegisterDto.UserName.ToLowerInvariant();
+			}
 
-            if (await _authRepository.UserExist(userForRegisterDto.UserName))
-            {
-                ModelState.AddModelError("UserName", "Username already exist");
-            }
+			if (await _authRepository.UserExist(userForRegisterDto.UserName))
+			{
+				ModelState.AddModelError("UserName", "Username already exist");
+			}
 
-            // TODO: Validate request
-            if(!ModelState.IsValid) 
-            {
-                return BadRequest(ModelState);
-            }
+			// TODO: Validate request
+			if(!ModelState.IsValid) 
+			{
+				return BadRequest(ModelState);
+			}
 
-            var userToCreate = new User { UserName = userForRegisterDto.UserName };
-            var createdUser = await _authRepository.Register(userToCreate, userForRegisterDto.Password);
+			var userToCreate = new User { UserName = userForRegisterDto.UserName };
+			var createdUser = await _authRepository.Register(userToCreate, userForRegisterDto.Password);
+			var userToReturn = _mapper.Map<UserForListDto>(createdUser);
+			return CreatedAtRoute("GetUser", new { contoller = "Users", id = userToReturn.Id}, userToReturn);
+		}
 
-            return StatusCode(201);
-        }
+		[HttpPost("login")]
+		public async Task<IActionResult> Login([FromBody] UserLoginDto userLoginDto)
+		{
+			var dbUser = await _authRepository.Login(userLoginDto.UserName.ToLowerInvariant(), userLoginDto.Password);
 
-        [HttpPost("login")]
-        public async Task<IActionResult> Login([FromBody] UserLoginDto userLoginDto)
-        {
-            var dbUser = await _authRepository.Login(userLoginDto.UserName.ToLowerInvariant(), userLoginDto.Password);
+			if(dbUser == null)
+			{
+				return Unauthorized();
+			}
 
-            if(dbUser == null)
-            {
-                return Unauthorized();
-            }
+			var user = _mapper.Map<UserForListDto>(dbUser);
 
-            var token = _tokenService.GenerateToken(dbUser.Id, dbUser.UserName);
-            return Ok(new {token});
-        }
-    }
+			var token = _tokenService.GenerateToken(dbUser.Id, dbUser.UserName);
+			return Ok(new {token, user});
+		}
+	}
 }
